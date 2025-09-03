c3b1f7ec5f5896544f9769771eb9f13dbb3866f4
lsc <lsc@maltego.com>
2025-09-03 11:20:02 +0200

    Bug Fix

--- experiments/data/__init__.py
+++ experiments/data/__init__.py
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85f966bedf845f7c3e4a36a5d498a05f84cf0b52a3bbc5a399ff0c710eb8d28b
-size 84
+from typing import Literal 
+
+supported_source = Literal["Synthetic", "AOL", "CAIDA"]
